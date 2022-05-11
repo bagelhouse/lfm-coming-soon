e19ea7a9e084d5710029fe252c44d8c238425af4
Michael Angelo <55844504+michaelangrivera@users.noreply.github.com>
2022-05-10 21:15:59 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState} from 'react'
+import backgroundphoto from './background.jpg';
 import './App.css';
+import Box from '@mui/material/Box'
+import Grow from '@mui/material/Grow'
+// @ts-ignore
+import TypeWriterEffect from 'react-typewriter-effect'
+
+const picSize = {
+  marginTop: '4%',
+  width: '80%',
+  height: '80%',
+  '@media (min-width:1024px)': {
+    height: '27%',
+    width: '27%'
+  }
+}
+
+const loadDelay = 2.5
 
 function App() {
+  const [start,
+    setStart] = useState(false);
+  
+    useEffect(() => {
+      let timer1 = setTimeout(() => setStart(true), loadDelay)
+  
+      // this will clear Timeout when component unmount like in willComponentUnmount
+      // and show will not change to true
+      return () => {
+        clearTimeout(timer1);
+      }
+    }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Grow
+      in={start}
+      style={{
+      transformOrigin: '0 0 0'
+    }}
+      {...(start ? { timeout: 2500 } : {})}>
+      <Box
+      component="img"
+      maxWidth = "sm"
+      // width = '90%'
+      // height = '90%' 
+      sx = {picSize}
+      
+      alt="The house from the offer."
+      src={backgroundphoto}
+      borderRadius = {30}
+      />
+      </Grow>
+      <TypeWriterEffect
+            textStyle={{ fontFamily: 'Comic Sans MS' , marginTop: '10px'}}
+            cursorColor="black"
+            multiText={[
+              'Coming Soon...',
+              'Winter 2020',
+            ]}
+            startDelay={2000}
+            typeSpeed={100}
+          />
       </header>
     </div>
   );
